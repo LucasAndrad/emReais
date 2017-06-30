81897820b7a937ee96d2773b94e137cdd9da1cce
Lucas Andrade <lucasandradeunb@gmail.com>
2017-06-30 00:38:31 -0300

    Create conversion for libra and euro

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,11 +3,16 @@ class PagesController < ApplicationController
   def home
     @dolar_value = 1
     @one_dolar = InoxConverter.convertCurrency(1, "USD", "BRL").round(2)
+
+    @euro_value = 1
+    @one_euro = InoxConverter.convertCurrency(1, "EUR", "BRL").round(2)
+
+    @libra_value = 1
+    @one_libra = InoxConverter.convertCurrency(1, "GBP", "BRL").round(2)
   end
 
   def get_dolar_value(dolar)
     @new_dolar = InoxConverter.convertCurrency(dolar, "EUR", "BRL").round(2)
-
   end
 
 end
