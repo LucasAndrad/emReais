e6fed1ac2dd1071cb3e5b1e1eef617ca598d0d70
Lucas Andrade <lucasandradeunb@gmail.com>
2017-08-02 13:17:35 -0300

    finish layouts details

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,17 @@
 class PagesController < ApplicationController
 
   def home
-    dolar = Coin.find_by(code: "USD")
-    @one_dolar = dolar.value.round(2)
+    @dolar = Coin.find_by(code: "USD")
+    @libra = Coin.find_by(code: "GBP")
+    @euro = Coin.find_by(code: "EUR")
 
-    euro = Coin.find_by(code: "EUR")
-    @one_euro = euro.value.round(2)
-
-    libra = Coin.find_by(code: "GBP")
-    @one_libra = libra.value.round(2)
+    if @dolar.value == nil || @euro.value == nil || @libra.value == nil
+      redirect_to error_path
+    else
+      @one_dolar = @dolar.value.round(2)
+      @one_euro = @euro.value.round(2)
+      @one_libra = @libra.value.round(2)
+    end
   end
 
   def get_dolar_value(dolar)
